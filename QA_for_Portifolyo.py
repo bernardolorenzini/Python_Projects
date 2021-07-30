517965c2ad74f83ddda68f6037ac5f6f45bf5603
Bernardo <lorenzinibernardo@gmail.com>
2021-07-30 00:26:10 -0300

    Create QA_for_Portifolyo.py
    On going project using selenium for chromedriver.
    
    Will test the quality of the portifolyo.

---
+++ QA_for_Portifolyo.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+PATH = "/Users/bernardolorenzini/Documents/Python_Projects/chromedriver"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://lorenzinibernardo.wixsite.com/bezini")
