98efc709f37b5b62d8ea8d7869111caff4d4d1b4
Bernardo <lorenzinibernardo@gmail.com>
2021-07-30 13:20:01 -0300

    Update QA_for_Portifolyo.py
    QA test.
    
    Testing Contato in Portifolyo and adding text for sending message.

--- QA_for_Portifolyo.py
+++ QA_for_Portifolyo.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
 PATH = "/Users/bernardolorenzini/Documents/Python_Projects/chromedriver"
 
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://lorenzinibernardo.wixsite.com/bezini")
+print(driver.title)
+
+search_contato = driver.find_element_by_id("DrpDwnMn03label")
+search_contato.click()
+
+search_nome = driver.find_element_by_id("input_comp-kemp07a71")
+search_email = driver.find_element_by_id("input_comp-kemp07af2")
+search_tel = driver.find_element_by_id("input_comp-kemp07ah1")
+search_msg = driver.find_element_by_id("textarea_comp-kemp07aj")
+
+search_nome.send_keys("Bernardo Lorenzini")
+search_email.send_keys("lorenzinibernardo@gmail.com")
+search_tel.send_keys("55984455207")
+search_msg.send_keys("ESSE e UM TESTE")
+
+search_botao = driver.find_element_by_id("comp-kemp07ap4")
+search_botao.click()
+
+time.sleep(5)
+driver.quit()
