45213124af58d6a6e93291f2dc26aebfe07fcbae
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2021-08-27 14:28:13 -0300

    criando teste de conexao entre programas python e mysql para futuros projetos

--- teste_servidor.py
+++ teste_servidor.py
@@ -4,11 +4,11 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="admin506606",
-    database="testdatabase"
+    database="condominio_turin"
 )
 
 mycursor = db.cursor()
-# CRIANDO DATABASE TESTE  ----  mycursor.execute("CREATE DATABASE testdatabase")
+#CRIANDO DATABASE TESTE  ---- mycursor.execute("CREATE DATABASE condominio_turin")
 
 # CRIANDO TABELA PESSOAS ----- mycursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
 
@@ -16,9 +16,13 @@ mycursor = db.cursor()
 
 #  mycursor.execute("INSERT INTO Person(name, age) VALUES (%s,%s)",("Bernardo", 29))
 
-mycursor.execute("INSERT INTO Person(name, age) VALUES (%s,%s)",("Maria", 26))
+mycursor.execute("INSERT INTO Predio (apID) VALUES (601)")
+mycursor.execute("INSERT INTO Predio (apID) VALUES (602)")
+mycursor.execute("INSERT INTO Predio (apID) VALUES (603)")
+mycursor.execute("INSERT INTO Predio (apID) VALUES (604)")
 db.commit()
-mycursor.execute("SELECT * FROM Person")
 
-for x in  mycursor:
-    print(x)
+#mycursor.execute("INSERT INTO Person(name, age) VALUES (%s,%s)",("Maria", 26))
+#db.commit()
+#mycursor.execute("SELECT * FROM Person")
+
