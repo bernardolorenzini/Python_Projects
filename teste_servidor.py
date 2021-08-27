ec5cb0dc4ff652b9be3549831df9bc7f92952ba8
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2021-08-27 01:52:31 -0300

    criando teste de conexao entre programas python e mysql para futuros projetos

--- teste_servidor.py
+++ teste_servidor.py
@@ -1,7 +1,24 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost:3306",
+    host="localhost",
     user="root",
-    passwd="admin506606"
+    passwd="admin506606",
+    database="testdatabase"
 )
+
+mycursor = db.cursor()
+# CRIANDO DATABASE TESTE  ----  mycursor.execute("CREATE DATABASE testdatabase")
+
+# CRIANDO TABELA PESSOAS ----- mycursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+
+# DESCREVENDO TABELA CRIADA ---- mycursor.execute("DESCRIBE Person")
+
+#  mycursor.execute("INSERT INTO Person(name, age) VALUES (%s,%s)",("Bernardo", 29))
+
+mycursor.execute("INSERT INTO Person(name, age) VALUES (%s,%s)",("Maria", 26))
+db.commit()
+mycursor.execute("SELECT * FROM Person")
+
+for x in  mycursor:
+    print(x)
